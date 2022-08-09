d32aa74cfa5aac9300e178fd0d892dbbbd838780
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2022-08-09 14:35:24 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ import About from './About';  
+ import Home from './Home'; 
+ import Serive from './Service';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">  
+         <Home />
+         <About /> 
+         <Serive />
+    </div>
+  );
+}
+
+export default App;
